3bfa4165ba46ab14204d74df05575c8ced7cd50e
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-11 10:02:35 -0700

    created department seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+-- insert into department table
+INSERT INTO department(name)
+VALUES ('Executive'),
+('Legal'),
+('Accounting'),
+('Engineering'),
+('Human Resources');
+
+
+-- insert into role table
+
+
+
+-- insert into employee table
