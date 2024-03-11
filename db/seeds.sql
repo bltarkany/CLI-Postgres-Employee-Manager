d533f22d048edad2e30d962fa48458ea4ae264fa
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-11 10:18:23 -0700

    created employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,5 +16,11 @@ VALUES ('CEO', 120000, 1),
 ('HR Consultant', 90000, 5);
 
 -- insert into employee table
--- INSERT INTO employee(first_name, last_name, hire_date, role_id, manager_id)
--- VALUES ('Executive'),
+-- date in postgres is YYYY-MM-DD
+INSERT INTO employee(first_name, last_name, hire_date, role_id, manager_id)
+VALUES ('Rick', 'Sanchez', '1901-02-10', 1, null),
+('Beth', 'Smith', '1981-10-14', 4, 1),
+('Morty', 'Smith Jr', '1999-05-04', 5, 2),
+('Summer', 'Smith', '1989-06-11', 2, 1),
+('Tammy', 'Gueterman', '1989-09-03', 3, null),
+('Bird', 'Person', '1912-12-25', 1, null);
