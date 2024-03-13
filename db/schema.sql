6cda9dab90600bdbd314c4739102db13b8961f94
Bobbi Tarkany <bl.tarkany@gmail.com>
2024-03-12 22:59:18 -0700

    initial create tables

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,17 @@ CREATE DATABASE employee_manager_db;
 -- use database
 \c employee_manager_db;
 
+-- create department table
+CREATE TABLE department(
+    id SERIAL PRIMARY KEY
+);
+
+-- create role table
+CREATE TABLE role(
+    id SERIAL PRIMARY KEY
+);
+
+-- create employee table
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY
+);
